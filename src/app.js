5cd8ee096c20b42ef06f4cb080fc67ccb838ef62
Guilherme Alan Ritter <ritterg2@universalleaf.com>
2024-04-24 11:23:27 -0300

    attempt to fix to work when running in the VPS; TODO: understand build pipeline and do it the right way

--- src/app.js
+++ src/app.js
@@ -2,9 +2,50 @@
   Form / application "onsubmit" handler, and analytics.
 */
 
-import * as jsonabc from '/jsonabc';
+// import { sort } from 'jsonabc';
 
-window.appSort = appSort;
+function jsonabc_isPlainObject(val) { return Object.prototype.toString.call(val) === '[object Object]'; }
+function jsonabc_sortObj(un, noarray) {
+    noarray = noarray || false;
+    var or = {};
+    if (isArray(un)) {
+        if (noarray) { or = un; } else { or = un.sort(); }
+        or.forEach(function (v, i) { or[i] = jsonabc_sortObj(v, noarray); });
+        if (!noarray) {
+            or = or.sort(function (a, b) {
+                a = JSON.stringify(a); b = JSON.stringify(b);
+                return a < b ? -1 : (a > b ? 1 : 0);
+            });
+        }
+    } else if (jsonabc_isPlainObject(un)) {
+        or = {};
+        Object.keys(un).sort(function (a, b) {
+            if (a.replace("@","").toLowerCase() < b.replace("@","").toLowerCase()) return -1;
+            if (a.replace("@","").toLowerCase() > b.replace("@","").toLowerCase()) return 1;
+            return 0;
+        }).forEach(function (key) { or[key] = jsonabc_sortObj(un[key], noarray); });
+    } else { or = un; }
+    return or;
+}
+function jsonabc_cleanJSON(input) {
+    input = input.replace(/,[ \t\r\n]+}/g, '}');
+    input = input.replace(/,[ \t\r\n]+\]/g, ']');
+    return input;
+}
+function jsonabc_sort(inputStr, noarray) {
+    var output, obj, r; if (inputStr) {
+        try {
+            inputStr = jsonabc_cleanJSON(inputStr);
+            obj = JSON.parse(inputStr); r = jsonabc_sortObj(obj, noarray);
+            output = JSON.stringify(r, null, 4);
+        }
+        catch (ex) {
+            console.error('jsonabc: Incorrect JSON object.', [], ex);
+            throw ex;
+        }
+    }
+    return output;
+}
 
 function appSort (ev, tid) {
   var inputStr = document.getElementById(tid).value;
@@ -13,12 +54,13 @@ function appSort (ev, tid) {
   ev.preventDefault();
 
   try {
-    var output = jsonabc.sort(inputStr, noarray);
+    var output = jsonabc_sort(inputStr, noarray);
 
     document.getElementById(tid).value = output;
 
     console.warn('jsonabc input:', JSON.parse(inputStr), noarray);
   } catch (ex) {
+    console.log(ex);
     window.alert('Incorrect JSON object');
   }
 }
